99b756bbe501f5a0e3ef222ec3dc433b699921ef
Laughing-q <1185102784@qq.com>
2026-03-06 00:01:23 +0800

    Update nn/autobackend.py

--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -272,7 +272,9 @@ class AutoBackend(nn.Module):
             if len(self.names) == 999 and (self.task == "segment" or len(y) == 2):  # segments and names not defined
                 nc = y[0].shape[1] - y[1].shape[1] - 4  # y = (1, 32, 160, 160), (1, 116, 8400)
                 self.names = {i: f"class{i}" for i in range(nc)}
-            return self.from_numpy(y[0]) if len(y) == 1 else [self.from_numpy(x) for x in y]
+            y = self.from_numpy(y[0]) if len(y) == 1 else [self.from_numpy(x) for x in y]
+            print("before model:", y)
+            return y
         else:
             return self.from_numpy(y)
 
