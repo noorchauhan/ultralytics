75ca5b7f09a59151b30e957d12c9b5734c02af79
Laughing-q <1185102784@qq.com>
2026-03-05 23:55:47 +0800

    Update backends/mnn.py

--- ultralytics/nn/backends/mnn.py
+++ ultralytics/nn/backends/mnn.py
@@ -44,7 +44,7 @@ class MNNBackend(BaseBackend):
         Returns:
             Model output as list of MNN tensors.
         """
-        input_var = self.expr.const(int(im.data_ptr()), im.shape)
+        input_var = self.expr.const(im.data_ptr(), im.shape)
         output_var = self.net.onForward([input_var])
         y = [x.read() for x in output_var]
         print("raw:", y)
