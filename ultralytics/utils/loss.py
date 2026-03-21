cf9b1db18e6b33a75e4dde247d6cf25ec809974a
rick <rick@ultralytics.com>
2026-03-21 22:33:07 +0800

    Revert "Replace hard-margin triplet with soft-margin (softplus)"
    This reverts commit 82ace81f40ac7af5db2de67c96fe5030ef82d171.

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -1047,8 +1047,7 @@ class ReIDLoss:
         if valid.sum() == 0:
             return torch.tensor(0.0, device=features.device, requires_grad=True)
 
-        diff = hardest_pos[valid] - hardest_neg[valid]
-        triplet_loss = F.softplus(diff)  # soft-margin: log(1 + exp(dp - dn))
+        triplet_loss = F.relu(hardest_pos[valid] - hardest_neg[valid] + self.triplet_margin)
         return triplet_loss.mean()
 
 
