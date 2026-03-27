652653a1364b57525b1febc1c0c3b4699588312e
Laughing-q <1185102784@qq.com>
2026-03-27 16:26:49 +0800

    Update callbacks/wb.py

--- ultralytics/utils/callbacks/wb.py
+++ ultralytics/utils/callbacks/wb.py
@@ -151,7 +151,6 @@ def on_fit_epoch_end(trainer):
 
 def on_train_epoch_end(trainer):
     """Log metrics and save images at the end of each training epoch."""
-    print(trainer.label_loss_items(trainer.tloss, prefix="train"))
     wb.run.log(trainer.label_loss_items(trainer.tloss, prefix="train"), step=trainer.epoch + 1)
     wb.run.log(trainer.lr, step=trainer.epoch + 1)
     if trainer.epoch == 1:
